2577767983ce07eae4fd87894cb969c8ee43ab75
Hoang Duy Nguyen <duynguyenhoang1611@gmail.com>
2025-06-12 21:04:42 +0700

    neon db

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './db/schema';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 
-const client = postgres(process.env.DATABASE_URL!);
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
 
-export const db = drizzle(client, { schema });
 
