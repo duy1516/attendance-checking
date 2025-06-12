2c38912a32e174f9a29fe868ee2e2878c140fd55
Hoang Duy Nguyen <duynguyenhoang1611@gmail.com>
2025-06-12 21:51:12 +0700

    ko

--- src/db/index.ts
+++ src/db/index.ts
@@ -9,8 +9,6 @@
 
 // export const db = drizzle(queryClient);
 
-import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql });
+export const db = drizzle(process.env.DATABASE_URL!);
